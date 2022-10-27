7df199cb0a46975d6909ec00848ffd5663c0359a
Oggal <Philip.dan.taylor@gmail.com>
2022-10-27 13:13:10 -0700

    Correct Syntax of Header

--- main.h
+++ main.h
@@ -4,10 +4,10 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int printChar(char)
-int _printf(const char *format, ...)
-int printDouble(float)
-int printString(char*)
-int printInt(int)
+int printChar(char);
+	int _printf(const char *format, ...);
+	int printDouble(float);
+	int printString(char *);
+	int printInt(int);
 
 #endif
