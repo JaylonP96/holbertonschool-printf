1ed4a11eac6a8ab52ea7c13994f3eec1e362d3b0
JaylonP96 <jaylonp96@gmail.com>
2022-10-27 13:54:59 -0700

    add file print.c

---
+++ printString.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * printString - printing string provided
+ *
+ * Return - number of characters printed
+ */
+int printString(char *s)
+{
+	int i = 0;
+while ( s[i] ) 
+ {
+	 printChar s[i];
+	 i++;
+	}
+return (i);
+}
+
