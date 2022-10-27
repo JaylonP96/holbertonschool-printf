cff3060cfe01e8ac70afc9e6609b3cfed1c9734e
Philip <Philip.dan.taylor@gmail.com>
2022-10-27 10:31:21 -0700

    Adds _printf.c
    Includes initial design of printf

--- _printf.c
+++ _printf.c
@@ -8,6 +8,43 @@
  */
 int _printf( char *format, ...)
 {
+	va_list args;
+	unsigned int charSum = 0, index = 0;
+
+	while (format[index] != '\0')
+	{
+		while (format[index] != '%')
+		{
+			sum += printChar(format[index]);
+			index++;
+		}
+		/*
+		 * Getting this far ensures the current char is %
+		 * Next is decieding what we should be doing
+		 */
+		switch (format[index + 1])
+		{
+		case 'c':
+			charSum += printChar(va_arg(args, int));
+			break;
+		case 's':
+			charSum += printString(va_arg(args, char *));
+			break;
+		case '%':
+			charSum += printChar('%');
+			break;
+		case 'i':
+			charSum += printInt(va_arg(args, int));
+			break;
+		case 'd':
+			charSum += printDouble(va_arg(args, int));
+			break;
+
+		}
+		/* Increment index past the format char */
+		index +=2;
+	}
+
 	/*NOT IMPLEMENTED*/
-	return (-1);
+	return ((int)charSum);
 }
