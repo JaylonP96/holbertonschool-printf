15b2c2a409f4c8cda46c6db6e39337a44547a43f
JaylonP96 <jaylonp96@gmail.com>
2022-10-27 10:44:32 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+int printChar(char)
+int _printf(const char *format, ...)
+int printDouble(float)
+int printString(char*)
+int printInt(int)
+
+#endif
