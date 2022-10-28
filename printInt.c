59f6497900236a8db6b0b432c5df1b861259bd95
Oggal <Philip.dan.taylor@gmail.com>
2022-10-28 10:06:41 -0700

    Implement printInt function body

--- printInt.c
+++ printInt.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * printInt - currently place holder
+ * printInt - Print Int Value
+ *@i: Int Value to print
  *
- * Returning - number passed to it
+ * Return: cout of bytes wrote
  */
 int printInt(int i)
 {
-     return (i);
+	int o = 0;
+	if (i < 10)
+	{
+		return printChar('0' +i);
+	}
+	else
+	{
+		o = printInt(i / 10);
+		return o + printChar('0' + (i % 10));
+	}
 }
