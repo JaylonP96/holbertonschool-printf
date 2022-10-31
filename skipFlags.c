662a4bb92b52b15842ea3ac6789358521d163e21
Oggal <Philip.dan.taylor@gmail.com>
2022-10-31 09:44:38 -0700

    Correct Typo

--- skipFlags.c
+++ skipFlags.c
@@ -7,9 +7,9 @@ int skipFlags(int *index, const char *format)
 	char flags[] = {'#', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 			'-', '+', ' ', 'h', 'l', 'L', 'j', 'z', 't', '\0'};
 
-	for (flagIndex = 0; flag[flagIndex]; flagIndex++)
+	for (flagIndex = 0; flags[flagIndex]; flagIndex++)
 	{
-		if (flag[flagIndex] == format[*index])
+		if (flags[flagIndex] == format[*index])
 		{
 			(*index)++;
 			flagIndex = 0;
