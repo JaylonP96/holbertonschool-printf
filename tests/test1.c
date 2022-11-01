b0f0d5ca05f68ce6a8cd65a74fd1274b35f00ddd
Oggal <Philip.dan.taylor@gmail.com>
2022-11-01 12:03:42 -0700

    Tweaks to Test1

--- tests/test1.c
+++ tests/test1.c
@@ -9,19 +9,35 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (i == 'n')
+			i++;
 		out[1] = i;
 		printf("\n\n\t\t TEST CASE %c",i);
-		printf("\nInt values\n");
-		printf(out,0);
-		printf(out,-5);
-		printf(out,INT_MIN);
-		printf(out, INT_MAX);
-		printf("\n");
-		_printf(out,0);
-		_printf(out,-5);
-		_printf(out,INT_MIN);
-		_printf(out, INT_MAX);
+		if (i != 's')
+		{
+			printf("\nInt values\n");
+			printf(out,0);
+			printf(out,-5);
+			printf(out,INT_MIN);
+			printf(out, INT_MAX);
+			printf("\n");
+			_printf(out,0);
+			_printf(out,-5);
+			_printf(out,INT_MIN);
+			_printf(out, INT_MAX);
 
+			printf("\nChar values\n");
+			printf(out,'c');
+			printf(out, CHAR_MAX);
+			printf(out, SCHAR_MIN);
+			printf(out, SCHAR_MAX);
+			printf("\n");
+			_printf(out,'c');
+			_printf(out, CHAR_MAX);
+			_printf(out, SCHAR_MIN);
+			_printf(out, SCHAR_MAX);
+
+		}
 	        printf("\nString values\n");
 		printf(out,"");
 		printf(out,NULL);
